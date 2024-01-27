8819cbcf63b1e31fa68a2fc801ca79bef27a90ff
waiyanhtun282 <waiyanhtun282@gmail.com>
2024-01-27 21:57:03 +0630

    ADD_USER_MODEL_DATA

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app =express();
+const connectDB =require('./config/db');
 
 app.get('/',(req,res) => res.send('API running'));
+
+// connectDB
+connectDB();
+
+// Define Route
+
+ app.use('/api/users', require('./routes/api/users'));
+ app.use('/api/auth', require('./routes/api/auth'));
+ app.use('/api/posts', require('./routes/api/posts'));
+ app.use('/api/profile', require('./routes/api/profile'));
+
+
 
 const PORT =process.env.PORT || 5000
 
